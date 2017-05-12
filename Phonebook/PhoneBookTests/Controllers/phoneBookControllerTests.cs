e8b5422376d402e565b268466f1da55f74909094
andredonandre <andreowen10@gmail.com>
2017-05-12 14:52:39 +0200

    Created the tests for the PhonebookController

--- Phonebook/PhoneBookTests/Controllers/phoneBookControllerTests.cs
+++ Phonebook/PhoneBookTests/Controllers/phoneBookControllerTests.cs
@@ -5,84 +5,224 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace Phonebook.Tests
 {
     [TestClass()]
     public class phoneBookControllerTests
     {
+        public phoneBookController pbCtrl = new phoneBookController();
+
         [TestMethod()]
         public void phoneBookControllerFunctionTest()
         {
-            Assert.Fail();
+            Assert.IsNotNull(pbCtrl);
         }
 
         [TestMethod()]
-        public void CreateFunctionTest()
+        public void CreateFunction_AddsContact()
         {
-            Assert.Fail();
+            //Act
+            Contact contact = new Contact { Name = "Eliud Kipchoge", PhoneNumber = "07267398400", Address = "45 Taita Hills"};
+            var initialCount = pbCtrl.phonebook.Contacts.Count;
+            pbCtrl.Create(contact);
+            var result = pbCtrl.phonebook.Contacts.Count;
+            //Assert
+            Assert.AreNotEqual(result, initialCount);
         }
 
         [TestMethod()]
-        public void CreateFunctionTest1()
+        public void CreateFunction_AddsUserGroup()
         {
-            Assert.Fail();
+            //Act
+            UserGroup usergroup = new UserGroup("Kompis"); 
+            var initialCount = pbCtrl.phonebook.UserGroups.Count;
+            pbCtrl.Create(usergroup);
+            var result = pbCtrl.phonebook.UserGroups.Count;
+            //Assert
+            Assert.AreNotEqual(result, initialCount);
         }
 
         [TestMethod()]
-        public void DeleteFunctionTest()
+        public void DeleteFunction_RemovesContact()
         {
-            Assert.Fail();
+            //Act
+            var contact = pbCtrl.phonebook.Contacts.First();
+            var initialCount = pbCtrl.phonebook.Contacts.Count;
+            pbCtrl.Delete(contact);
+            var result = pbCtrl.phonebook.Contacts.Count;
+            //Assert
+            Assert.AreNotEqual(result, initialCount);
         }
 
         [TestMethod()]
-        public void DeleteFunctionTest1()
+        public void DeleteFunction_RemovesSpecificContact()
         {
-            Assert.Fail();
+            //Act
+            var contact = pbCtrl.phonebook.Contacts.First();
+            pbCtrl.Delete(contact);
+            var result = pbCtrl.phonebook.Contacts.Where(x=> x.Id == contact.Id).FirstOrDefault();
+            //Assert
+            Assert.IsNull(result);
         }
 
         [TestMethod()]
-        public void GetContactsFunctionTest()
+        public void DeleteFunction_RemovesUserGroup()
         {
-            Assert.Fail();
+            //Act
+            var usergroup = pbCtrl.phonebook.UserGroups.First();
+            var initialCount = pbCtrl.phonebook.UserGroups.Count;
+            pbCtrl.Delete(usergroup);
+            var result = pbCtrl.phonebook.UserGroups.Count;
+            //Assert
+            Assert.AreNotEqual(result, initialCount);
         }
 
         [TestMethod()]
-        public void GetUserGroupsFunctionTest()
+        public void DeleteFunction_RemovesSpecificUserGroup()
         {
-            Assert.Fail();
+            //Act
+            var usergroup = pbCtrl.phonebook.UserGroups.Where(x => x.Name != "All Contacts" || x.Name != "Unassigned").FirstOrDefault();
+            pbCtrl.Delete(usergroup);
+            var result = pbCtrl.phonebook.UserGroups.Where(x => x.Id == usergroup.Id).FirstOrDefault();
+            //Assert
+            Assert.IsNull(result);
         }
 
         [TestMethod()]
-        public void UpdateFunctionTest()
+        public void GetContactsFunction_NotNull()
         {
-            Assert.Fail();
+            //Act
+            var result = pbCtrl.GetContacts();
+            //Assert
+            Assert.IsNotNull(result);
         }
 
         [TestMethod()]
-        public void UpdateFunctionTest1()
+        public void GetContactsFunction_NoContacts_EmptyList()
         {
-            Assert.Fail();
+            //Act
+            var result = new Collection<Contact>();
+            //Assert
+            Assert.IsNotNull(result);
         }
 
         [TestMethod()]
-        public void GetObjectFunctionTest()
+        public void GetUserGroupsFunction_NotNull()
         {
-            Assert.Fail();
+            //Act
+            var result = pbCtrl.GetUserGroups();
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        public void GetContactsFunction_NoUserGroups_EmptyList()
+        {
+            //Act
+            var result = new Collection<UserGroup>();
+            //Assert
+            Assert.IsNotNull(result);
         }
 
         [TestMethod()]
-        public void GetObjectFunctionTest1()
+        public void UpdateFunction_UpdatesContact_Name()
         {
-            //Declaring method to be tested
-            Assert.Fail();
+            //ACt
+            var old = pbCtrl.phonebook.Contacts.FirstOrDefault();
+            var change = new Contact { Name = "Walshy Fire", PhoneNumber = old.PhoneNumber, Address = old.Address, userGroup = old.userGroup };
+            pbCtrl.Update(old, change);
+            var newVersion = pbCtrl.phonebook.Contacts.Where(c => c.Id == old.Id).FirstOrDefault();
+            //Assert
+            Assert.AreEqual(newVersion.Name, change.Name);
         }
 
         [TestMethod()]
-        public void GetObjectFunctionTest2()
+        public void UpdateFunction_UpdatesContact_PhoneNumber()
         {
-            //Declaring method to be tested
-            Assert.Fail();
+            //Act
+            var old = pbCtrl.phonebook.Contacts.FirstOrDefault();
+            var change = new Contact { Name = old.Name, PhoneNumber = "08888888888", Address = old.Address, userGroup = old.userGroup };
+            pbCtrl.Update(old, change);
+            var newVersion = pbCtrl.phonebook.Contacts.Where(c => c.Id == old.Id).FirstOrDefault();
+            //Assert
+            Assert.AreEqual(newVersion.PhoneNumber, change.PhoneNumber);
+        }
+
+        [TestMethod()]
+        public void UpdateFunction_UpdatesContact_Address()
+        {
+            //Act
+            var old = pbCtrl.phonebook.Contacts.FirstOrDefault();
+            var change = new Contact { Name = old.Name, PhoneNumber = old.PhoneNumber, Address = "45 Sky Avenue, Mars", userGroup = old.userGroup };
+            pbCtrl.Update(old, change);
+            var newVersion = pbCtrl.phonebook.Contacts.Where(c => c.Id == old.Id).FirstOrDefault();
+            //Assert
+            Assert.AreEqual(newVersion.Address, change.Address);
+        }
+
+        [TestMethod()]
+        public void UpdateFunction_UpdatesContact_UserGroup()
+        {
+            //Act
+            var old = pbCtrl.phonebook.Contacts.FirstOrDefault();
+            var change = new Contact { Name = old.Name, PhoneNumber = old.PhoneNumber, Address = old.Address, userGroup = new UserGroup("The Crew")};
+            pbCtrl.Update(old, change);
+            var newVersion = pbCtrl.phonebook.Contacts.Where(c => c.Id == old.Id).FirstOrDefault();
+            //Assert
+            Assert.AreEqual(newVersion.userGroup, change.userGroup);
+        }
+
+        [TestMethod()]
+        public void UpdateFunction_UpdatesUserGroup_Name()
+        {
+            //Act
+            var old = pbCtrl.phonebook.UserGroups.FirstOrDefault();
+            var change = new UserGroup("The Crew");
+            pbCtrl.Update(old, change);
+            var newVersion = pbCtrl.phonebook.UserGroups.Where(c => c.Id == old.Id).FirstOrDefault();
+            //Assert
+            Assert.AreEqual(newVersion.Name, change.Name);
+        }
+
+        [TestMethod()]
+        public void GetObjectFunction_ContactObjectPresent_NotNull()
+        {
+            //Act
+            var contact = pbCtrl.phonebook.Contacts.FirstOrDefault();
+            var result = pbCtrl.GetObject(contact);
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void GetObjectFunction_ContactObjectNotPresent_Null()
+        {
+            //Act
+            var contact = new Contact { Name = "Eliud Kipchoge", PhoneNumber = "07267398400", Address = "45 Taita Hills" };
+            var result = pbCtrl.GetObject(contact);
+            //Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod()]
+        public void GetObjectFunction_UserGroupObjectPresent_NotNull()
+        {
+            //Act
+            var userGroup = pbCtrl.phonebook.UserGroups.FirstOrDefault();
+            var result = pbCtrl.GetObject(userGroup);
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void GetObjectFunction_UserGroupObjectNotPresent_Null()
+        {
+            //Act
+            UserGroup usergroup = new UserGroup("Kompis");
+            var result = pbCtrl.GetObject(usergroup);
+            //Assert
+            Assert.IsNull(result);
         }
     }
 }
